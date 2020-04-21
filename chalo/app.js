b0d6fc3c9d8457554b46ed828096797e518bdaf9
karsivakumar <sivakumar.karthik@gmail.com>
2020-04-21 14:25:28 -0500

    bootstrap / jquery / header and footer

--- chalo/app.js
+++ chalo/app.js
@@ -4,14 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var layouts = require('express-ejs-layouts');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var contactRouter = require('./routes/contact');
+var helpRouter = require('./routes/help');
+var privpolicyRouter = require('./routes/privpolicy');
+var $ = require('jquery');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(layouts);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,6 +29,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/help', helpRouter);
+app.use('/privacy', privpolicyRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+
+const routeResponseMap = {
+	"/about": "<h1> About Simple Application</h1>",
+	"/contact": "<h1> Contact Us </h1>",
+	"/error": "<h1> Sorry - cannot find what you are looking for. </h1>"
+};
+
 var http = require('http');
 var port = 4100;
+var fs = require('fs');
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Welcome To Chalo's First Nodejs Page \n" );
+    if (routeResponseMap[req.url]) {
+	    res.end(routeResponseMap[req.url]);
+    }
+    else {
+ 	    res.end("Welcome to Simple Application!");
+    }
+
     console.log('Processed request for '+ req.url);
 }).listen(port);
 console.log('Test web application server running ...');
